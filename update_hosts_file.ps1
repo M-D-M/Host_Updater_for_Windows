ff39e77a066eb602819af313c53bbe9d447a7743
M-D-M <m.d.mcnamara@gmail.com>
2016-12-10 13:09:13 -0500

    Esclate to Admin and Promp
    Updated script to escalate to Admin level and also prompt user for
    actions when launched from Windows explorer.

--- update_hosts_file.ps1
+++ update_hosts_file.ps1
@@ -9,6 +9,8 @@ $scriptDesc="Update hosts file with $scriptName"
 $taskName="UpdateHosts"
 
 function main ($vals) {
+    escalatePriv
+    
     if ($vals.length -lt 1)
     {
         usageMessage
@@ -37,7 +39,40 @@ function main ($vals) {
 }
 
 function usageMessage {
-    write-host "`nUsage: <script name> [install|uninstall|run|status]"
+    $title = "Host Updater for Windows"
+    $message = "Welcome to the host updater for Windows.  Please choose an option below:"
+
+    $choice1 = New-Object System.Management.Automation.Host.ChoiceDescription "&Install", `
+        "Installs host updater task and updates host file."
+
+    $choice2 = New-Object System.Management.Automation.Host.ChoiceDescription "&Uninstall", `
+        "Uninstalls host updater task and reverts host file to original."
+
+    $choice3 = New-Object System.Management.Automation.Host.ChoiceDescription "&Run", `
+        "Updates host file without installing host updater task."
+
+    $choice4 = New-Object System.Management.Automation.Host.ChoiceDescription "&Status", `
+        "Reports status of host file and host updater task."
+
+    $choice5 = New-Object System.Management.Automation.Host.ChoiceDescription "&Exit", `
+        "Exits this script."
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($choice1, $choice2, $choice3, $choice4, $choice5)
+
+    $result = $host.ui.PromptForChoice($title, $message, $options, 4) 
+
+    switch ($result)
+    {
+        0 { installUpdateAgent }
+        1 { uninstallUpdateAgent }
+        2 { replaceHosts }
+        3 { checkStatus }
+        4 {}
+    }
+
+    # write-host "`nUsage: $scriptName [install|uninstall|run|status]"
+
+    waitForUser
 }
 
 function replaceHosts {
@@ -87,9 +122,6 @@ function installUpdateAgent {
 
     # Add exclusion path for Windows Defender so it won't undo changes to hosts file
     Add-MpPreference -ExclusionPath "$hosts_file_loc"
-
-    write-host "Done! Press any key to continue..."
-    Read-Host 
 }
 
 function uninstallUpdateAgent {
@@ -113,11 +145,6 @@ function uninstallUpdateAgent {
     Remove-MpPreference -ExclusionPath "$hosts_file_loc"
 }
 
-function outputHostsSize {
-    $hostsSize=$(Get-Content $hosts_file_loc | Measure-Object -Line).Lines
-    write-host "`nSize of hosts file: $hostsSize"
-}
-
 function checkStatus {
     if (!(isHostsFileModified)) {
         write-host "`nHosts file has not been modified."
@@ -132,6 +159,48 @@ function checkStatus {
     else {
         write-host "`nTask to update hosts file does not exist."
     }
+}
+
+function escalatePriv {
+    # Get the ID and security principal of the current user account
+    $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+    # Get the security principal for the Administrator role
+    $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+    # Check to see if we are currently running "as Administrator"
+    if ($myWindowsPrincipal.IsInRole($adminRole))
+    {
+        # We are running "as Administrator" - so change the title and background color to indicate this
+        $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+        $Host.UI.RawUI.BackgroundColor = "DarkRed"
+        clear-host
+    }
+    else
+    {
+       # We are not running "as Administrator" - so relaunch as administrator
+   
+       # Create a new process object that starts PowerShell
+       $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+       # Specify the current script path and name as a parameter
+       $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+   
+       # Indicate that the process should be elevated
+       $newProcess.Verb = "runas";
+   
+       # Start the new process
+       [System.Diagnostics.Process]::Start($newProcess);
+   
+       # Exit from the current, unelevated, process
+       exit
+    }
+}
+
+function outputHostsSize {
+    $hostsSize=$(Get-Content $hosts_file_loc | Measure-Object -Line).Lines
+    write-host "`nSize of hosts file: $hostsSize"
 }
 
 function doesAgentExist {
@@ -149,6 +218,11 @@ function isHostsFileModified {
     }
 
     return $returnVal
+}
+
+function waitForUser {
+    write-host "`nDone! Press any key to continue..."
+    Read-Host 
 }
 
 main $args
